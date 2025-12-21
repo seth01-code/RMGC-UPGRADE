8609e3b979b3e86ebbb901d3849602348f25d8dd
Prideof_All <sethikhielea@gmail.com>
2025-12-21 20:48:42 +0100

    com

--- src/app/admin/messages/page.tsx
+++ src/app/admin/messages/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { JSX, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import newRequest from "../../utils/newRequest";
 import { useRouter } from "next/navigation";
