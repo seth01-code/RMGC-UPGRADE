77da87c8eaeb153bbfe0af6fe46652d182b290d0
seth01-code <sethikhielea@gmail.com>
2026-06-30 16:43:36 +0100

    com

--- src/app/utils/newRequest.ts
+++ src/app/utils/newRequest.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://api.renewedmindsglobalconsult.com/",
+  baseURL: "https://api.renewedmindsglobalconsult.com/api",
   withCredentials: true,
 });
 
